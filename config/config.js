a125aed925afafbaa6d719412fcdaa31f35627db
JuanSilva2001 <79462202+JuanSilva2001@users.noreply.github.com>
2021-09-30 13:42:23 -0300

    Update config.js

--- config/config.js
+++ config/config.js
@@ -26,7 +26,7 @@ module.exports = {
   dev: {
     // altere APENAS username, password e database.
     username: 'root',
-    password: 'Saopaulo18',
+    password: 'NaoTemSenha',
     database: 'libertadores',
     host: 'localhost',
     dialect: 'mysql',
